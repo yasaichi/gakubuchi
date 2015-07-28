bbf13a33f511a675cc5f833e950f4b475750c584
yasaichi <yasaichi@users.noreply.github.com>
2015-07-28 22:33:50 +0900

    fix some files to test in multiple versions of Rails 4.x

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,12 @@ require 'pry-byebug'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+rails_version = Gem::Version.create(Rails.version)
+if rails_version < Gem::Version.create('4.1.0')
+  ActiveRecord::Migration.check_pending!
+elsif rails_version >= Gem::Version.create('4.1.0')
+  ActiveRecord::Migration.maintain_test_schema!
+end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
